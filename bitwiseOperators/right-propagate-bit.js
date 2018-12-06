00c4c9ecd7e66bdd2e29c4d139fa862e63d2c682
Chase Armstrong <I21664@xactware.com>
2018-12-06 08:43:04 -0700

    Add in learning points for right propagate bit function

--- bitwiseOperators/right-propagate-bit.js
+++ bitwiseOperators/right-propagate-bit.js
@@ -1,7 +1,13 @@
 /*
-Time complexity is O(1) because regardless of the input size, we're
-performing a constant number of operations. Space complexity here is O(1)
-because we're storing a constant number of variables in memory.
+    Time complexity is O(1) because regardless of the input size, we're
+    performing a constant number of operations. Space complexity here is O(1)
+    because we're storing a constant number of variables in memory.
+
+    The learning for me here comes from two insights. First, if we minus one from
+    any number, the right most bit set to one becomes a zero and every bit right of
+    that bit becomes a one. This gets us most of the way there, with only the original
+    right most one bit now set to zero. Combining that with the isolated right most
+    bit (using XOR) gives allows us to add back in that bit.
 */
 
 const rightPropagateBitV1 = (num) => {
