b90b92ebda8041385c71d555efce7cdc7489791b
Chase Armstrong <I21664@xactware.com>
2018-12-06 08:45:49 -0700

    Add in learning for is power of two function

--- bitwiseOperators/is-power-of-two.js
+++ bitwiseOperators/is-power-of-two.js
@@ -2,6 +2,12 @@
     Time complexity here is O(1) because we perform a constant amount
     of operations, regardless of the input size. Space complexity is O(1)
     as well.
+
+    The learning here is that every number to the power of two will only have one
+    bit that is set to one and every other bit should be set to zero. Therefore, we
+    can isolate the right most bit set to one and then compare it against the origal
+    number. If it has changed, there were multiple bits set to one. If they are equal
+    and there is at least one bit set to one, we have a number that is a power of two.
 */
 
 const isPowerOfTwo = (num) => {
